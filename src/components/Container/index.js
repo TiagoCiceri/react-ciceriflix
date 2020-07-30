135c0880e981e3e4b063f1ae98b1b913fa9553cb
TiagoCiceri <tiagociceri@gmail.com>
2020-07-30 20:14:50 -0300

    Dia 4 finalizado

---
+++ src/components/Container/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.main`
+    padding: 15px 35px;
+    max-width: auto;
+    background: #4d2600;
+    height: auto;
+    width: auto;
+   /* display: flex;
+    flex-direction: column;    */
+    /*align-items: center;*/
+    /*justify-content: center;*/
+`;
+
+const Container = ({ children }) => (
+    <StyledContainer>{children}</StyledContainer>
+)
+
+export default Container
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
-
+import Button from '../../../components/Button';
+import Container from '../../../components/Container';
+import '../Cadastro.css';
 
 function CadastroCategoria(){
     const dadosCategoria = {
-        nome: '',
-        descricao: '',
-        cor: '#000',
-    };
+        id: '',
+        titulo: '',
+        cor: '',
+    }
+
     const [categorias, setCategorias] =  useState([]);
     const [dados, setDados] = useState(dadosCategoria);
 
@@ -31,52 +34,77 @@ function CadastroCategoria(){
         setValue(dadosCategoria);
     }
 
+    useEffect(() => {
+        if (window.location.href.includes('localhost')){
+            const URL = 'http://localhost:8080/categorias';
+
+            fetch(URL)
+                .then(async (respostaServer) => {
+                    if (respostaServer.ok){
+                        const dadosConvertidos = await respostaServer.json();
+                        console.log(dadosConvertidos);
+                        setCategorias(dadosConvertidos);
+                        return;
+                    }
+                    throw new Error('Não foi possível pegar os dados');
+                })
+        }
+    },[])
+
     return(
         <PageDefault>
-            <h2>Cadastro de categoria: {dados.nome}</h2>
+            <Container>
+                <div className="Row">
+                    <div className="Column"> 
+                        <h2>Cadastro de categoria: 
+                            <strong style={{ color: '#DB202C'}}>
+                                {' '+dados.titulo}
+                            </strong> 
+                        </h2>
 
-            <form onSubmit={handleSubmit}>
-                <FormField 
-                    label="Nome: "
-                    type="text" 
-                    name="nome" 
-                    value={dados.nome}
-                    onChange={handleChange}
-                />
+                        <form onSubmit={handleSubmit}>
+                            <FormField 
+                                label="Título: "
+                                type="text" 
+                                name="titulo" 
+                                value={dados.titulo}
+                                onChange={handleChange}
+                            />
 
-                <FormField 
-                    label="Descrição: "
-                    type="text" 
-                    name="descricao" 
-                    value={dados.descricao}
-                    onChange={handleChange}
-                />
+                            <FormField 
+                                label="Cor: "
+                                type="color" 
+                                name="cor" 
+                                value={dados.cor}
+                                onChange={handleChange}
+                            />
 
-                <FormField 
-                    label="Cor: "
-                    type="color" 
-                    name="cor" 
-                    value={dados.cor}
-                    onChange={handleChange}
-                />
+                            <Button className="ButtonCad">
+                                Cadastrar
+                            </Button>
 
-                <button>Cadastrar</button>
+                        </form>
+                    </div>
 
-            </form>
-
-            <ul>
-                {categorias.map((categoria, indice) =>{
-                    return(
-                        <li key={`${categoria}${indice}`}>
-                            {categoria.nome}
-                        </li>
-                    )
-                })}
-            </ul>
-
+                    <div className="Column">
+                        <h3>Categorias gravadas:</h3>
+                        <ul>
+                            {categorias.map((categoria, indice) =>{
+                                return(
+                                    <li key={categoria.id}>
+                                        {categoria.titulo}
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </Container>
+            
             <Link to="/">
                 Voltar para Home.
             </Link>
+
         </PageDefault>
     )
 };
